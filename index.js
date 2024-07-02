ea12d997e66795cde8fbfa75c816f1f377c4208a
favour uchechi <favouruche093@gmail.com>
2024-07-02 13:45:15 +0100

    made a few changes on javascript

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-function updateTimeUTC() {
 let time = new Date().toUTCString();
 
 document.querySelector("[data-testid='currentTimeUTC']").innerHTML = time;
-}
 
-setInterval(updateTimeUTC, 1000);
-
-updateTimeUTC();
 
 const daysOfTheWeek = [
   "Sunday",
